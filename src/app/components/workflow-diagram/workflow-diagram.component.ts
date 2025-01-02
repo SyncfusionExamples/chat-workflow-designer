4af0ef1aaa7389e8945f211853c440dfe5aabca8
GowthamKumarMSF4219 <gowthamkumar.murugavel@syncfusion.com>
2025-01-02 10:17:34 +0530

    Feature(402675):sidebar show

--- src/app/components/workflow-diagram/workflow-diagram.component.ts
+++ src/app/components/workflow-diagram/workflow-diagram.component.ts
@@ -9,7 +9,7 @@ import { BeforeOpenCloseMenuEventArgs, DropDownButtonComponent, DropDownButtonMo
 import { CommonModule } from '@angular/common';
 import { ListViewComponent, ListViewModule, SelectEventArgs } from '@syncfusion/ej2-angular-lists';
 import { LIST_DATA } from '../../data/list-data';
-import { ClickEventArgs, ContextMenuComponent, ContextMenuModule, MenuComponent, MenuItemModel, MenuModule, ToolbarModule } from '@syncfusion/ej2-angular-navigations';
+import { ClickEventArgs, ContextMenuComponent, ContextMenuModule, MenuComponent, MenuEventArgs, MenuItemModel, MenuModule, ToolbarModule } from '@syncfusion/ej2-angular-navigations';
 import { DropDownListModule, MultiSelectModule } from '@syncfusion/ej2-angular-dropdowns';
 import { NumericTextBoxModule, TextAreaModule, TextBoxModule, UploaderModule } from '@syncfusion/ej2-angular-inputs';
 import { DatePickerModule, DateTimePickerModule } from '@syncfusion/ej2-angular-calendars';
@@ -243,14 +243,9 @@ export class WorkflowDiagramComponent implements AfterViewInit {
   }
 
   // Method to add a new node and connect it
-  // public onDiagramRefresh(): void {
-  //   this.diagram.setProperties({ nodes: [], connectors: [] }, true);
-  //   this.diagram.refresh();
-  // }
-
   public onDiagramRefresh(): void {
     this.diagram.setProperties({ nodes: [], connectors: [] }, true);
-      this.diagram.refresh();
+    this.diagram.refresh();
   }
 
   // on node delete 
@@ -266,6 +261,15 @@ export class WorkflowDiagramComponent implements AfterViewInit {
         console.log("Delete failed");
       }
     });
+  }
+
+  public onClickAddBtn(option : any, index : string){
+    console.log(option + "  \n" + index)
+    var addbtn=document.getElementById('addbtn');
+    var addPos = addbtn.getBoundingClientRect();
+    this.contextmenu.open(addPos.top + window.scrollY, addPos.left+window.scrollX);
+    this.diagram.selectedItems.userHandles[1].visible = false;
+    this.diagram.selectedItems.userHandles[2].visible = false;
   }
 
   public onUserHandleMouseDown(event: UserHandleEventsArgs) {
@@ -298,55 +302,24 @@ export class WorkflowDiagramComponent implements AfterViewInit {
     }
   }
 
-  public onOpenDropDownButton(args: OpenCloseMenuEventArgs, option: any | null, index: number | null) {
-    console.log("Drop :" + args.element);
-    if (index != null) {
-      this.dropdownbutton.toggle();
-      if (this.diagram.selectedItems.userHandles) {
-        this.diagram.selectedItems.userHandles[1].visible = false;
-        this.diagram.selectedItems.userHandles[2].visible = false;
-      }
-    }
-    else {
-      let dropDownContainer = document.querySelector('.dropDown-container') as HTMLElement;
-      args.element.parentElement!.style.top = dropDownContainer.getBoundingClientRect().top + dropDownContainer.offsetHeight + 'px';
-
-      let ulElement = document.querySelector('ul') as HTMLElement;
-      args.element.parentElement!.style.left = dropDownContainer.getBoundingClientRect().left - (ulElement.getBoundingClientRect().width / 2) + (dropDownContainer.getBoundingClientRect().width / 2) + 'px';
+  public onCloseContextMenu(args: BeforeOpenCloseMenuEventArgs) {
+    if(!this.isParentListItem && !((args.event.target as HTMLElement).closest(".e-menu-item")==null)) {
+        this.sidebarComponent?.sidebar?.show();
     }
   }
 
-  public onBeforeCloseDropDownButton(args: BeforeOpenCloseMenuEventArgs) {
-    args.cancel = this.isParentListItem;
-    if (!(this.isParentListItem || ((args.event.target as HTMLElement).closest(".bc-block-option.e-listview")==null))) {
-      this.sidebarComponent?.sidebar?.show();
-    }
-  }
-
-  public onSelectListView(args: SelectEventArgs) {
-    this.isParentListItem = args.item.classList.contains("e-has-child");
-    const selectedItem = args.item;
-    const selectedItemId = selectedItem.getAttribute('data-uid');
-    const selectedItemText = args.item ? args.item.textContent : null;
-    if (selectedItemId && /^0[1-3]$/.test(selectedItemId)) { // Check if the ID is '01', '02', or '03'
+  public itemSelect(args: MenuEventArgs): void {
+    this.isParentListItem = args.item.items.length != 0;
+    if (args.item.id && /^0[1-3]$/.test(args.item.id)) { // Check if the ID is '01', '02', or '03'
+      const selectedItemText = args.item ? args.item.text : null;
       this.sidebarHeader = selectedItemText ? selectedItemText.trim() + ' Block' : '';
     }
-    if(!this.isParentListItem && typeof args?.data === 'object' && 'editerTypeId' in args.data) {
-      this.nodeBlockType = (args.data as { blockid: number })['blockid'];
-      this.nodeEditType = (args.data as { editerTypeId: number })['editerTypeId'];
+    if(args.item.items.length == 0){
+      this.nodeBlockType = (args.item as { blockid: number })['blockid'];
+      this.nodeEditType = (args.item as { editerTypeId: number })['editerTypeId'];
     }
   }
-
-  public onBeforeOpenDropDownButton() {
-    this.isParentListItem = false; // The value is reset here, to handle document click case of dropdown
-    // Reset ListView to its initial state before opening
-    if (this.listView) {
-      while ((this.listView as any).curDSLevel.length > 0) {
-        this.listView.back();
-      }
-    }
-  }
-
+  
   public onClicked(args: ClickEventArgs) {
     if (args.item.id === 'save') {
       this.download(this.diagram.saveDiagram());
